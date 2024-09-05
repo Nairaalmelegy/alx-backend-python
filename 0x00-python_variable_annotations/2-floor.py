585942ddfa9512930dd041213b2b8d1ac8126ea1
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-09-05 11:41:56 +0300

    adding floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Basic annotations """
+
+
+def floor(n: float) -> float:
+    """Type annotated a function to return the floor
+    Args:
+    n: float which will be turned into floor
+    Returns:
+    integer version of floor of n
+    """
+    return int(n)
