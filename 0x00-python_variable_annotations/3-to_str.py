08bc6a85ffc273d0f2f5ee79a5e6c5a306f05a77
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-09-05 11:46:30 +0300

    adding the to str function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Basic annotations """
+
+
+def to_str(n: float) -> str:
+    """Type annotated which this function turns n into string
+    Args:
+    n: float number given
+    Return:
+    string representation of n
+    """
+    return str(n)
