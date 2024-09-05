c1f00233c997e7a4d7cefa435c14dfd7a2615f0a
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-09-05 11:38:55 +0300

    adding concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic annotations """
+
+
+def concat(str1: str, str2: str) -> str:
+    """Type annotated for concatinating 2 strings
+    Args:
+    str1: first string
+    str2: second string
+    Return:
+    sum of two strings next to each others
+    """
+    return str1 + str2
