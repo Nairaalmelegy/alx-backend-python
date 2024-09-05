719406e5ac1ed8f473822395ac248f0a750f8932
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-09-05 11:54:12 +0300

    sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Complex types and list floats """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Typ annotated a fnction which
+    Args:
+    input_list: float
+    Return:
+    sum of the float
+    """
+    return sum(input_list)
