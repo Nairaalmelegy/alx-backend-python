878dd09d7eeda5cc7f05e6a04ff4808e3bd393e8
Nairaalmelegy <nairamohamed384@yahoo.com>
2024-09-05 12:13:23 +0300

    adding get value function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,8 +5,8 @@ F = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping,
-        key: Any,
-        default: Union[F, None]) -> Union[Any, F]:
+                     key: Any,
+                     default: Union[F, None]) -> Union[Any, F]:
     """ Type-annotated function safe_first_element. """
     if key in dct:
         return dct[key]
